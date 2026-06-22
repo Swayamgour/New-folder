71265d237a9026bbff61bd363ff6ce5467012440
Swayam Gaur <goura0775@gmail.com>
2026-06-22 14:09:47 +0530

    lkj

--- npclproject/src/App.jsx
+++ npclproject/src/App.jsx
@@ -15,6 +15,7 @@ import image2 from './assets/images/unnamed(2).webp';
 import image3 from './assets/images/unnamed(3).webp';
 import image4 from './assets/images/unnamed(4).webp';
 
+
 // Placeholder images – replace with your actual imports
 const images = [image, image1, image2, image3, image4];
 
